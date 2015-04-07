5f7d156c2f9cd6237fbc724d99f534f62fbaf3b9
Andrei Motinga <motingandrei7@gmail.com>
2015-04-07 11:30:28 -0400

    andrei

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,16 @@
 Rails.application.routes.draw do
-  
-  resources :concerns
+  get 'sessions/new'
 
+  get 'sessions/create'
+
+  get 'sessions/destroy'
+
+  root  'welcome#index'
+  get '/login', to: 'sessions#new'
+  get '/auth/facebook/callback', to: 'sessions#create'
+  get '/logout', to: 'sessions#destroy'
+
+  resources :concerns
   resources :users
 
 end
