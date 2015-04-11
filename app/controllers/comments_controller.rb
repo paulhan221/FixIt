2e7e7b179bf59aa37abc6340e126ebc36f4c1a89
Paul Han <paulhan221@gmail.com>
2015-04-11 18:01:57 -0400

    fixed comments to display commenter instead of current user

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -10,6 +10,6 @@ class CommentsController < ApplicationController
 	private
 
 	def comment_params
-		params.require(:comment).permit(:content)
+		params.require(:comment).permit(:content, :user_id)
 	end
 end
