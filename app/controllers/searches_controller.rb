a7d8dd2f3743970d8eeb44d7416dae3f4fed223a
ngfromusa <ngfromusa@gmail.com>
2015-04-13 09:06:44 -0400

    search fixed

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,7 +1,7 @@
 class SearchesController < ApplicationController
   def index
     @results = Search.for(params[:keyword])
-    @results = @results.sort_by(&:total_votes).reverse
+    @results = @results.sort_by(&:total).reverse
 
   end
 end
